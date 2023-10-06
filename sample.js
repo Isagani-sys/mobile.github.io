9314e9de4bbcb1d7d850a10e8c6601a94b857821
Isagani-sys <129724436+Isagani-sys@users.noreply.github.com>
2023-10-07 02:33:19 +0800

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,20 +1,12 @@
 const searchInput = document.getElementById('search');
-const container = document.querySelector('.container');
+const searchContainer = document.getElementById('search-container');
 
 searchInput.addEventListener('focus', () => {
-    const scrollY = window.scrollY;
-    const containerTop = container.getBoundingClientRect().top;
-    const inputTop = searchInput.getBoundingClientRect().top;
-    const offset = inputTop - containerTop;
-    
-    // Check if the input field is obscured by the keyboard
-    if (offset > window.innerHeight / 2) {
-        const scrollAmount = offset - (window.innerHeight / 2);
-        container.style.transform = `translateY(-${scrollAmount}px)`;
-    }
+    // Add an "active" class to the search bar when it's focused
+    searchContainer.classList.add('active');
 });
 
 searchInput.addEventListener('blur', () => {
-    // Reset the container position when the input loses focus
-    container.style.transform = 'translateY(0)';
+    // Remove the "active" class when the input loses focus
+    searchContainer.classList.remove('active');
 });
