3f7a5cb9fdc54180a1394fbf17f652739a5b0ea4
Isagani-sys <129724436+Isagani-sys@users.noreply.github.com>
2023-10-07 02:36:14 +0800

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,12 +1,10 @@
 const searchInput = document.getElementById('search');
-const searchContainer = document.getElementById('search-container');
+const container = document.querySelector('.container');
 
 searchInput.addEventListener('focus', () => {
-    // Add an "active" class to the search bar when it's focused
-    searchContainer.classList.add('active');
+    container.classList.add('active');
 });
 
 searchInput.addEventListener('blur', () => {
-    // Remove the "active" class when the input loses focus
-    searchContainer.classList.remove('active');
+    container.classList.remove('active');
 });
